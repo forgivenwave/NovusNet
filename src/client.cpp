2de3d3568734fd54e1e90bcb69df7a1bf3cef597
Nullora <nullora@tutamail.com>
2026-03-05 18:53:32 +0100

    Added clear comments

--- src/client.cpp
+++ src/client.cpp
@@ -3,9 +3,11 @@
 
 int main(){
     std::string msg;
+    //runClient(ip,port) connects to a server
     runClient("127.0.0.1", 9090);
     while(true){
         std::getline(std::cin,msg);
+        //sendMsg(string msg) sends data as a string
         sendMsg(msg);
     }
     return 0;
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,13 @@
 #include <chrono>
 int main(){
     runServer(9090);
+    //chrono so loop doesn't tank CPU usage
     while(!clientConnected){
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    //after clientConnected, receive simple messages
     while(true){
+        //recvMsg() returns a string.
         std::cout<<recvMsg()<<"\n";
     }
     return 0;
