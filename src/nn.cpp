e52ffcdb43ce90850a6d63c9754272ab0f55480a
Nullora <nullora@tutamail.com>
2026-03-29 16:38:01 +0100

    readded cool comment at top

--- src/nn.cpp
+++ src/nn.cpp
@@ -1,3 +1,7 @@
+//This library is made by @Nullora on Github. The link can be found here, and documentation aswell: https://github.com/Nullora/NovusNet
+//NovusNet is a c++ networking library made to facilitate connection between devices while keeping it fast and secure.
+//It's fully free and anyone can distribute/use it. im testing stuff rn.
+//Last updated: 27/3/26
 #include "nn.hpp"
 #include <string>
 #include <map>
